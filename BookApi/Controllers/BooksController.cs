7fe1a5b849d7de3a9d6cb72d529c010c84b172db
gggray000 <ggg_108@hotmail.com>
2025-08-17 12:54:25 +0800

    Until lesson 17.

--- BookApi/Controllers/BooksController.cs
+++ BookApi/Controllers/BooksController.cs
@@ -1,6 +1,24 @@
-﻿namespace BookApi.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using BookApi.Models;
+
+namespace BookApi.Controllers
 {
-    public class BooksController
+    //localhost:7056/api/books
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
     {
+        private Book[] _books = new Book[]
+        {
+           new Book{Id = 1, Author = "Author One", Title = "Book One"},
+           new Book{Id = 2, Author = "Author Two", Title = "Book Two"},
+           new Book{Id = 3, Author = "Author Three", Title = "Book Three"},
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(_books);
+        }
     }
 }
